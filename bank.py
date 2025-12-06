18acf8ab3e07d9af48d9c23f247528ee7ffa4efe
theHrDev <hamzatrukayat2822@gmail.com>
2025-12-06 22:21:32 +0100

    Added the deposit function

--- bank.py
+++ bank.py
@@ -2,9 +2,10 @@ class BankAccount:
     owner = "ayo"
     balance = 0
     
-    def __init__(self,owner,amount):
+    def __init__(self,owner,amount,deposit_amount):
         self.owner = owner
         self.withdraw(amount)
+        self.deposit(deposit_amount)
         
     def withdraw(self,amount):
         self.amount= amount
@@ -14,13 +15,19 @@ class BankAccount:
             print(self.balance)
             self.balance = self.balance - amount
             print(f"your balance is {self.balance}")
-        pass
+        
     
-    def deposit(self):
-        pass
+    def deposit(self,deposit_amount):
+        if deposit_amount < 0:
+            print("Negative value")
+        else:
+            self.balance = self.balance + deposit_amount
+            
+            
 owner = input("Enter your name: ")
 amount = int(input("Enter the amount you want to withdraw: "))
-BankAccount(owner,amount)
+deposit_amount = int(input("Enter the amount you want to deposit: "))
+BankAccount(owner,amount,deposit_amount)
         
         
         
