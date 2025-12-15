dadbc3b67ce47fa4ca0899f2bdec45a5762344ff
theHrDev <hamzatrukayat2822@gmail.com>
2025-12-15 16:24:42 +0100

    Added the user interactive menu

--- library.py
+++ library.py
@@ -11,27 +11,25 @@ class Library:
         
         
 def librarian_action():
-    print("\n--- Library Menu ---")
-    print("1. Add Books")
-    print("2. List Books")
-    
-    choice = input("Choose an option (1-2): ")
-   
-    
     librarian = Library()
     
-    if choice == "1":
-        book = input("Enter the name of the book: ")
-        author = input("Enter the book author: ")
-        print(librarian.add_books(book=book,author=author))
-    if choice == "2":
-        print(librarian.list_books())
-        
-    repeat = input("Do you want to another thing\n1.yes\n2.no: ")
-    if repeat == "1":
-        librarian_action()
-    else:
-        exit()
+
+    while(True):
+        print("\n--- Library Menu ---")
+        print("1. Add Books")
+        print("2. List Books")
+        choice = input("Choose an option (1-2): ")
+    
+        if choice == "1":
+            book = input("Enter the name of the book: ")
+            author = input("Enter the book author: ")
+            print(librarian.add_books(book=book,author=author))
+        if choice == "2":
+            print(librarian.list_books())
+            
+        break
+            
+  
     
 
 librarian_action()
