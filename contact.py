5a840307872604e65f41d4587d11e9682d82c210
theHrDev <hamzatrukayat2822@gmail.com>
2025-12-26 17:56:08 +0100

    Added the menu function for each method in the class

--- contact.py
+++ contact.py
@@ -15,7 +15,7 @@ class ContactBook:
         
         if name not in self.contact_book:
             id = random.randint(10**2,10**3+1)
-            new_contact = {name,contact,id}
+            new_contact = {"name":name,"contact":contact,"id":id}
             self.contact_book[id] = new_contact
             return f"{name} added successfully to the contact book"
         return "There is a contact with the existing name"
@@ -41,7 +41,7 @@ class ContactBook:
         return self.contact_book
     
     def edit(self,id,title,value):
-        if id not in self.contact_book():
+        if not self.contact_book[id]:
             return "contact with the given id does not exist"
         
         selected_contact = self.contact_book[id]
@@ -60,25 +60,33 @@ def contact_book():
         print("----- 1. Add contact ----")
         print("----- 2. Remove contact ----")
         print("----- 3. Edit contact -----")
-        print("----- 4. View all contacts ----")
-        print("----- 5. To exit ----")
+        print("----- 4. View single contact ----")
+        print("----- 5. View all contacts ----")
+        print("----- 6. To exit ----")
         
         choice = input("\nEnter the menu: ")
         if choice == "1":
             name = input("Enter the name: ")
             number = input(f"Enter the number of {name}: ")
-            if name or number == "":
-                print("Enter a valid value")
+            # if name or number == "":
+            #     print("Enter a valid value")
+            # else:
             print(contact.add_contact(name=name,contact=number))
         elif choice == "2":
             id = int(input("Enter the id for the contact to be deleted: "))
             print(contact.remove_contact(id))
         elif choice == "3":
             id = int(input("Enter the id for the contact to be view: "))
-            print(contact.view_single_contact(id=id))
+            title = input("Enter the key you want to edit: ")
+            value = input("Enter the value for the key: ")
+            print(contact.edit(id=id,value=value,title=title))
         elif choice == "4":
-            print(contact.view_all_contact())
+            id = int(input("Enter the id for the contact to be view: "))
+            print(contact.view_single_contact(id=id))
         elif choice == "5":
+            print(contact.view_all_contact())
+        elif choice == "6":
+            print("bye")
             break
         else:
             print("Invalid input")
